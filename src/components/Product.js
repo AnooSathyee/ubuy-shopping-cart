7088c238d560d3822508afbbd64c87441efff71b
Anu <anoosathyee@gmail.com>
2021-02-18 15:30:42 -0600

    partial setup done

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ export default class Product extends Component {
   render() {
     return (
       <div>
-        <h3>This is product page</h3>
+        <h3>This is the product from ubuy page</h3>
       </div>
     );
   }
